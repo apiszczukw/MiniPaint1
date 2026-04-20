12ea379571bacb64988558a78347b14f014767c9
apiszczukw <apiszczukw@gmail.com>
2026-04-20 12:53:10 +0200

    Implementacja funkcji rysowania prostych linii

---
+++ MiniPaint1/Klasy/Plotno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPaint1.Klasy
+{
+    internal class Plotno : IDrawable
+    {
+        public List<Linia> Linie { get; set; } = new();
+
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.FillColor = Colors.White;
+            canvas.FillRectangle(dirtyRect);
+
+
+            foreach (var linia in Linie)
+            {
+                canvas.StrokeColor = linia.Kolor;
+                canvas.StrokeSize = 2;
+                canvas.DrawLine(linia.Start, linia.Koniec);
+            }
+        }
+    }
+
+
+    public class Linia
+    {
+        public PointF Start { get; set; }
+
+        public PointF Koniec { get; set; }
+
+        public Color Kolor { get; set; } = Colors.Black;
+    }
+}
--- MiniPaint1/MainPage.xaml.cs
+++ MiniPaint1/MainPage.xaml.cs
@@ -1,14 +1,68 @@
-﻿namespace MiniPaint1
+﻿using MiniPaint1.Klasy;
+
+namespace MiniPaint1
 {
     public partial class MainPage : ContentPage
     {
+        Plotno plotno;
+
+        PointF punktStartowy;
+
+        Color pedzel = Colors.Black;
+
+        bool czyRysuje = false;
+
+        Linia? ostatniRysunek;
+
 
         public MainPage()
         {
             InitializeComponent();
+
+            plotno = new();
+            poleRysowania.Drawable = plotno;
         }
 
-       
+        private void poleRysowania_StartInteraction(object sender, TouchEventArgs e)
+        {
+            czyRysuje = true;
+            punktStartowy = e.Touches[0];
+        }
+
+        private void poleRysowania_DragInteraction(object sender, TouchEventArgs e)
+        {
+            if (!czyRysuje) return;
+
+            if(ostatniRysunek != null)
+            {
+                plotno.Linie.Remove(ostatniRysunek);
+            }
+
+            PointF punktAktualny = e.Touches[0];
+
+            RysujLinie(punktAktualny);
+
+            poleRysowania.Invalidate();
+        }
+
+        private void RysujLinie(PointF punktAktualny)
+        {
+            var segment = new Linia()
+            {
+                Start = punktStartowy,
+                Koniec = punktAktualny,
+                Kolor = pedzel
+            };
+
+            plotno.Linie.Add(segment);
+            ostatniRysunek = segment;
+        }
+
+        private void poleRysowania_EndInteraction(object sender, TouchEventArgs e)
+        {
+            czyRysuje = false;
+            ostatniRysunek = null;
+        }
     }
 
 }
